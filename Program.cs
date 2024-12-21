064dec299a4d12070f1abc98ca1b45b94c773761
Евгений Кучма <desferto87@gmail.com>
2024-12-21 22:57:19 +0500

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace homeWorkSequence
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int steps = 7;
+            int numberMin = 5;
+            int numberMax = 103;
+
+            for (int i = numberMin; i <= numberMax; i += steps)
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
